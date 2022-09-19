fd21cf4d6111d637c2cbb5a0130e10c6cfb367bf
Mikael Freva <mikaelfreva@live.fr>
2022-09-19 18:13:05 +0200

    deploy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,119 +1,139 @@
-var addButton = document.getElementById("addButton");
-var addInput = document.getElementById("itemInput");
-var todoList = document.getElementById("todoList");
+var addButton = document.getElementById('addButton');
+var addInput = document.getElementById('itemInput');
+var todoList = document.getElementById('todoList');
 var listArray = [];
 
 function listItemObj(content, status) {
-  this.content = "";
-  this.status = "incomplet";
+    this.content = '';
+    this.status = 'incomplete';
 }
-var changeToComp = function () {
-  var parent = this.parentElement;
-  parent.className = "incomplet well";
-  this.innerText = "incomplet";
-  this.removeEventListener("click", changeToComp);
-  this.addEventListener("click", changeToInComp);
-  changeListArray(parent.firstChild.innerText, "complete");
-};
+var changeToComp = function(){
+    var parent = this.parentElement;
+    console.log('Changed to complete');
+    parent.className = 'uncompleted well';
+    this.innerText = 'Incomplete';
+    this.removeEventListener('click',changeToComp);
+    this.addEventListener('click',changeToInComp);
+    changeListArray(parent.firstChild.innerText,'complete');
 
-var changeToInComp = function () {
-  var parent = this.parentElement;
-  parent.className = "termine well";
-  this.innerText = "Complete";
-  this.removeEventListener("click", changeToInComp);
-  this.addEventListener("click", changeToComp);
+}
 
-  changeListArray(parent.firstChild.innerText, "incomplet");
-};
+var changeToInComp = function(){
+    var parent = this.parentElement;
+    console.log('Changed to incomplete');
+    parent.className = 'completed well';
+    this.innerText = 'Complete';
+    this.removeEventListener('click',changeToInComp);
+    this.addEventListener('click',changeToComp);
 
-var removeItem = function () {
-  var parent = this.parentElement.parentElement;
-  parent.removeChild(this.parentElement);
+    changeListArray(parent.firstChild.innerText,'incomplete');
 
-  var data = this.parentElement.firstChild.innerText;
-  for (var i = 0; i < listArray.length; i++) {
-    if (listArray[i].content == data) {
-      listArray.splice(i, 1);
-      refreshLocal();
-      break;
+}
+
+var removeItem = function(){
+    var parent = this.parentElement.parentElement;
+    parent.removeChild(this.parentElement);
+
+    var data = this.parentElement.firstChild.innerText;
+    for(var i=0; i < listArray.length; i++){
+
+        if(listArray[i].content == data){
+            listArray.splice(i,1);
+            refreshLocal();
+            break;
+        }
     }
-  }
-};
 
-var changeListArray = function (data, status) {
-  for (var i = 0; i < listArray.length; i++) {
-    if (listArray[i].content == data) {
-      listArray[i].status = status;
-      refreshLocal();
-      break;
+
+}
+
+var changeListArray = function(data,status){
+
+    for(var i=0; i < listArray.length; i++){
+
+        if(listArray[i].content == data){
+            listArray[i].status = status;
+            refreshLocal();
+            break;
+        }
     }
-  }
-};
+}
 
-var createItemDom = function (text, status) {
-  var listItem = document.createElement("li");
-  var itemLabel = document.createElement("label");
-  var itemCompBtn = document.createElement("button");
-  var itemIncompBtn = document.createElement("button");
-  listItem.className =
-    status == "incomplet" ? "termine well" : "incomplet well";
+var createItemDom = function(text,status){
 
-  itemLabel.innerText = text;
-  itemCompBtn.className = "btn btn-success";
-  itemCompBtn.innerText = status == "Incomplet" ? "Complete" : "Incomplet";
-  if (status == "incomplet") {
-    itemCompBtn.addEventListener("click", changeToComp);
-  } else {
-    itemCompBtn.addEventListener("click", changeToInComp);
-  }
+    var listItem = document.createElement('li');
 
-  itemIncompBtn.className = "btn btn-danger";
-  itemIncompBtn.innerText = "Delete";
-  itemIncompBtn.addEventListener("click", removeItem);
+    var itemLabel = document.createElement('label');
 
-  listItem.appendChild(itemLabel);
-  listItem.appendChild(itemCompBtn);
-  listItem.appendChild(itemIncompBtn);
+    var itemCompBtn = document.createElement('button');
 
-  return listItem;
-};
+    var itemIncompBtn = document.createElement('button');
 
-var refreshLocal = function () {
-  var todos = listArray;
-  localStorage.removeItem("todoList");
-  localStorage.setItem("todoList", JSON.stringify(todos));
-};
+    listItem.className = (status == 'incomplete')?'completed well':'uncompleted well';
 
-var addToList = function () {
-  var newItem = new listItemObj();
-  newItem.content = addInput.value;
-  listArray.push(newItem);
-  refreshLocal();
-  var item = createItemDom(addInput.value, "incomplet");
-  todoList.appendChild(item);
-  addInput.value = "";
-};
-
-var clearList = function () {
-  listArray = [];
-  localStorage.removeItem("todoList");
-  todoList.innerHTML = "";
-};
-
-window.onload = function () {
-  var list = localStorage.getItem("todoList");
-
-  if (list != null) {
-    todos = JSON.parse(list);
-    listArray = todos;
-
-    for (var i = 0; i < listArray.length; i++) {
-      var data = listArray[i].content;
-
-      var item = createItemDom(data, listArray[i].status);
-      todoList.appendChild(item);
+    itemLabel.innerText = text;
+    itemCompBtn.className = 'btn btn-success';
+    itemCompBtn.innerText = (status == 'incomplete')?'Complete':'Incomplete';
+    if(status == 'incomplete'){
+        itemCompBtn.addEventListener('click',changeToComp);
+    }else{
+        itemCompBtn.addEventListener('click',changeToInComp);
     }
-  }
+
+
+    itemIncompBtn.className = 'btn btn-danger';
+    itemIncompBtn.innerText = 'Delete';
+    itemIncompBtn.addEventListener('click',removeItem);
+
+    listItem.appendChild(itemLabel);
+    listItem.appendChild(itemCompBtn);
+    listItem.appendChild(itemIncompBtn);
+
+    return listItem;
+}
+
+var refreshLocal = function(){
+    var todos = listArray;
+    localStorage.removeItem('todoList');
+    localStorage.setItem('todoList', JSON.stringify(todos));
+}
+
+var addToList = function(){
+    var newItem = new listItemObj();
+    newItem.content = addInput.value;
+    listArray.push(newItem);
+    //add to the local storage
+    refreshLocal();
+    //change the dom
+    var item = createItemDom(addInput.value,'incomplete');
+    todoList.appendChild(item);
+    addInput.value = '';
+}
+
+//function to clear todo list array
+var clearList = function(){
+    listArray = [];
+    localStorage.removeItem('todoList');
+    todoList.innerHTML = '';
+
+}
+
+window.onload = function(){
+    var list = localStorage.getItem('todoList');
+
+    if (list != null) {
+        todos = JSON.parse(list);
+        listArray = todos;
+
+        for(var i=0; i<listArray.length;i++){
+            var data = listArray[i].content;
+
+            var item = createItemDom(data,listArray[i].status);
+            todoList.appendChild(item);
+        }
+
+    }
+
 };
-addButton.addEventListener("click", addToList);
-clearButton.addEventListener("click", clearList);
+addButton.addEventListener('click',addToList);
+clearButton.addEventListener('click',clearList);
